af3e04c5ecbe97d6c9835b65720c1863f3633f8f
AZEUS\rafha <rafhall@hotmail.com>
2024-12-14 23:28:52 +0000

    Creating window to display 3D graphics

--- OpenGL_3D_Rendering_1.5/main.cpp
+++ OpenGL_3D_Rendering_1.5/main.cpp
@@ -1,37 +1,67 @@
 #include <GLFW/glfw3.h>
 
-int main(void)
-{
-    GLFWwindow* window;
-
-    /* Initialize the library */
-    if (!glfwInit())
+int main() {
+    // Init GLFW
+    if (!glfwInit()) {
         return -1;
+    }
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    // Create a window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Test 3D Rendering Engine", NULL, NULL);
+    if (!window) {
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        // Render graphics here
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
+    // Cleaning up
+    glfwDestroyWindow(window);
     glfwTerminate();
+
     return 0;
 }
+
+// testing everything works below
+
+//int main(void)
+//{
+//    GLFWwindow* window;
+//
+//    /* Initialize the library */
+//    if (!glfwInit())
+//        return -1;
+//
+//    /* Create a windowed mode window and its OpenGL context */
+//    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+//    if (!window)
+//    {
+//        glfwTerminate();
+//        return -1;
+//    }
+//
+//    /* Make the window's context current */
+//    glfwMakeContextCurrent(window);
+//
+//    /* Loop until the user closes the window */
+//    while (!glfwWindowShouldClose(window))
+//    {
+//        /* Render here */
+//        glClear(GL_COLOR_BUFFER_BIT);
+//
+//        /* Swap front and back buffers */
+//        glfwSwapBuffers(window);
+//
+//        /* Poll for and process events */
+//        glfwPollEvents();
+//    }
+//
+//    glfwTerminate();
+//    return 0;
+//}
